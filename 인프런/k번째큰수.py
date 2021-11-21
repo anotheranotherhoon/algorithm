f89dafdbdf295583aecd6b4a6c7f7614c6333509
anotheranotherhoon <77476348+anotheranotherhoon@users.noreply.github.com>
2021-11-21 19:21:19 +0900

    211121start

---
+++ 인프런/k번째큰수.py
@@ -0,0 +1,10 @@
+n, s = map(int, input().split())
+a = list(map(int, input().split()))
+res=set()
+for i in range(n):
+	for j in range(i+1,n):
+		for m in range(j+1,n):
+			res.add(a[i]+a[j]+a[m])
+res=list(res)
+res.sort(reverse = True)
+print(res[s-1])
