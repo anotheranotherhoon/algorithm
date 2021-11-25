9feb2463242b3d37be4b87c4f0e1b648fc2f5342
anotheranotherhoon <77476348+anotheranotherhoon@users.noreply.github.com>
2021-11-26 03:39:29 +0900

    211125

--- AA.py
+++ AA.py
@@ -1,27 +1,15 @@
 import sys
 sys.stdin=open("input.txt", "rt")
-n=int(input())
-a=list(map(int, input().split()))
-lt=0
-rt=n-1
-last=0
-res=""
-tmp=[]
-while lt<=rt:
-	if a[lt]>last:
-		tmp.append((a[lt],"L"))
-	if a[rt]>last:
-		tmp.append((a[rt], "R"))
-	tmp.sort()
-	if len(tmp)==0:
-		break
+s=input()
+stack=[]
+cnt=0
+for i in range(len(s)):
+	if s[i]=='(':
+		stack.append(s[i])
 	else:
-		res=res+tmp[0][1]
-		last=tmp[0][0]
-		if tmp[0][1] == 'L':
-			lt+=1
+		stack.pop()
+		if s[i-1]=="(":
+			cnt+=len(stack) 
 		else:
-			rt-=1
-	tmp.clear
-print(len(res))
-print(res)
+			cnt+=1
+print(cnt)
